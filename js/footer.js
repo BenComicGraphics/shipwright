bcf4aa7ecfa1047e40404483e4632baaae164c10
ben carver <ben carver>
2017-04-05 18:36:45 -0500

    Signed-off-by: ben carver <ben carver>

--- js/footer.js
+++ js/footer.js
@@ -18,10 +18,6 @@ $(function() {
     $('.item').matchHeight(options);
 });
 */
-// WOW.js //
-    
-/* new WOW().init(); */
-    
     
 // Sticky Header functionality goes here //
 /*
@@ -37,47 +33,5 @@ $(function(){
 });
 */
 
-// Fancybox //
-/* 
-$(document).ready(function() {
-		$(".fancybox").fancybox();
-});
-*/
-
-
-// Fade-in Parallaxing //
-/*
-$(window).scroll( function(){
-  $('.slidein').each( function(i){
-    var bottom_of_object = $(this).position().top + $(this).outerHeight();
-    var bottom_of_window = $(window).scrollTop() + $(window).height();
-      if( bottom_of_window > bottom_of_object) {
-        $(this).animate({'opacity':'1'}, 1000);
-      }
-    });     
-  });    
-*/
-
-// Background Parallaxing //
-/*
-$(document).ready(function(){
-	//.parallax(xPosition, speedFactor, outerHeight) options:
-	//xPosition - Horizontal position of the element
-	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-	$('#target').parallax("50%", 0.1)
-})
-*/
-
-
-// Masonry Code //
-/*
-var $container = $('.blocks');
-// init
-$container.packery({
-  itemSelector: 'li',
-  gutter: 0
-});    
-*/
     
 });
