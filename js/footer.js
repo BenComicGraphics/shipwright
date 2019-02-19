109463e1bf17181079eaa43b92243e2c327fcc82
BenComicGraphics <benlcarver@gmail.com>
2019-02-18 19:52:18 -0600

    REmoved Bloat

--- js/footer.js
+++ js/footer.js
@@ -2,29 +2,9 @@ jQuery(function($) {
 
 /* Modifiable code starts here */
 
-// bxSlider Stuff Goes Here //
-
-$('.bxslider').bxSlider(); //
-
-// Tabs functionality goes here //
+$('.bxslider').bxSlider();
     
-// $( "#tabs" ).tabs(); }); //
+$( "#tabs" ).tabs();
 
-// Sticky Header functionality goes here //
-/*
-$(function(){
-	var stickyHeaderTop = $('#header-image').offset().top;
-		$(window).scroll(function(){
-        	if( $(window).scrollTop() > stickyHeaderTop ) {
-            	$('#page-head').addClass('navfixed');
-			} else {
-				$('#page-head').removeClass('navfixed');
-			}
-	});
-});
-*/
-  
-    
-cssVars();
     
 });  
